aae0ae0cd12fa431feac0721daf1ba2f7df5df95
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-10-20 13:32:36 -0400

    Initial commit

---
+++ section2/exercises/ex21.rb
@@ -0,0 +1,38 @@
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+  return a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  return a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  return a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+  return a / b
+end
+
+puts "Let's do some math with just functions!"
+
+age = add(30 , 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+#Answer to the puzzle
+# IQ (which is 50) divided by 2 = 25
+# Multiply that by weight which is 180: 25 * 180 = 4500
+# Subtract height - 4500: 74 - 4500 = -4426
+# Add age + -4426 = -4391
+# Lets see:
+puts "Here is a puzzle."
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+puts "That becomes: #{what}. Can you do it by hand?"
